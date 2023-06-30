7596784f9504b4bbdefbc0730bd807a44171c200
Rona C <rona.chen@hotmail.com>
2023-06-29 20:04:14 -0400

    added code for the database and install and debugged webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -29,13 +29,15 @@ module.exports = () => {
       }),
       // Creates a manifest.json file
       new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
         name: "Just Another Text Editor",
         short_name: "J.A.T.E",
         description: "Takes notes with JavaScript syntax highlighting!",
         background_color: "#225ca3",
         theme_color: "#225ca3",
-        start_url: "./",
-        publicPath: "./",
+        start_url: "/",
+        publicPath: "/",
         icons: [
           {
             src: path.resolve("src/images/logo.png"),
